2e749077c59c4b57e50b434dc2065dc2d66bc82b
ItsFishyy1 <117898431+ItsFishyy1@users.noreply.github.com>
2025-05-27 23:39:44 +0000

    added cards

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def index():
         ("Card 2 Title", "Some quick example text to build on the card title and make up the bulk of the card's content", "button Text", "static/images/reactorthing.png"),
         ("Card 3 Title", "Some quick example text to build on the card title and make up the bulk of the card's content", "button Text", "static/images/reactorthing.png"),
         ("Card 4 Title", "Some quick example text to build on the card title and make up the bulk of the card's content", "button Text", "static/images/reactorthing.png"),
-        ("Card 5 Title", "Some quick example text to build on the card title and make up the bulk of the card's content", "button Text", "static/images/reactorthing.png"),
+        ("Card 5 Title", "Some quick example text to build on the card title and make up the bulk of the card's content", "button Text", "static/images/reactorthing.png")
     
     )
-    return render_template("index.html"), 200
+    return render_template("index.html", cards=card_data), 200
 
 
 if __name__ == '__main__':
