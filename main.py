235f5968869c53d2b19ece981773444700c01f59
ItsFishyy1 <117898431+ItsFishyy1@users.noreply.github.com>
2025-06-22 23:41:00 +0000

    added more links

--- main.py
+++ main.py
@@ -31,5 +31,21 @@ def link1():
 def link2():
     return render_template("link2.html"), 200
 
+
+@app.route('/link3.html')
+def link3():
+    return render_template("link3.html"), 200
+
+
+@app.route('/donate.html')
+def donate():
+    return render_template("donate.html"), 200
+
+
+@app.route('/link4.html')
+def link4():
+    return render_template("link4.html"), 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
